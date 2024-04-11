73d9bf573e9f5eef45100b3e88c1079de220a3ed
AkashNimbalkar3430 <akashnimbalkar3430@gmail.com>
2024-04-11 12:39:56 +0530

    update file

--- aaaa.java
+++ aaaa.java
@@ -4,32 +4,9 @@ class aaaa
 public static void main(String args[])
 {
 
-//DSK;
 
-// ASK;
 
-System.out.println("HELLO AAAA");
 
-  int b=1000;
-
-  int c=b;
-
-  for(int i=1;i<=10;i++)
-    {
-    System.out.println(i);
-    }
-  
-System.out.println(c);
-//
-System.out.println("End Program Today");
-
-//
-
-//kumar;
-
-System.out.print("hi");
-
-  // prem;
 
 
 }
