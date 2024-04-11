7785617a05fd5cb01e716df88b2ac9bcd76b070c
AkashNimbalkar3430 <akashnimbalkar3430@gmail.com>
2024-04-11 12:44:17 +0530

    Update

--- aaaa.java
+++ aaaa.java
@@ -7,21 +7,6 @@ public static void main(String args[])
 
 String name ="Akash";
 
-// task i'm working currently 
-
-void m1 ()
-	{
-		int a=100;
-		
-	}
-
-// task i'm working currently 
-
-void m2 ()
-	{
-		int a=200;
-		
-	}
 
 
 
